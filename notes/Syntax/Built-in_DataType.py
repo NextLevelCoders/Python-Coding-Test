5100184991c03ab2da26cdc0f41cd2e429933e23
yb0x00 <yeongbeen0x00@gmail.com>
2024-09-14 00:07:41 +0900

    [코딩테스트합격자되기]~69p

---
+++ notes/Syntax/Built-in_DataType.py
@@ -0,0 +1,23 @@
+#built-in data type : int, float, char
+#collection data type : list, tuple, set, dic
+
+#정수형 산술 연산
+a = 2
+b = 3
+print(b/a)  #소수점 포함    1.5
+print(b//a) #몫 1
+print(b%a)  #나머지 1
+print(a**b) #a의 b승    8
+
+#정수형 비트 연산
+print(a&b)  #10 11  ->10    2
+print(a|b)  #->11   3
+print(a^b)  #xor(exclusive or,1개만 참일 경우 참)->01   1
+print(~a)   #보수
+print(a << 2)   #2=0010 ->1000=8
+print(a >> 1)   #->0001=1
+
+#정수형 논리 연산
+print(a and b)  #2
+print(a or b)   #3
+print(not a)    #False
